5c68a64db8ce4e9c1764b4104100f59c91f0d112
Heinrich Kruger <heindsight@kruger.dev>
2022-11-06 20:44:11 +0000

    Improve coverage measurement
    Use the [coverage-conditional-plugin](https://github.com/wemake-services/coverage-conditional-plugin)
    to conditionally exclude code from coverage measurement based on the
    tmuxp version.

--- rofi_tmuxp/tmuxp_client.py
+++ rofi_tmuxp/tmuxp_client.py
@@ -1,7 +1,7 @@
 """Tmuxp compatibility layer"""
 try:
     from tmuxp.config_reader import ConfigReader
-except ImportError:  # pragma: no cover
+except ImportError:  # pragma: has_tmuxp_1_16
     # In tmuxp < 1.16, 3d party library `Kaptan` was used to read config files.
     # `Kaptan` is just a wrapper around `yaml.safe_load` and `json.load`. Since Yaml is
     # a superset of JSON, we just use `yaml.safe_load` and wrap it in a class with
@@ -21,24 +21,24 @@ except ImportError:  # pragma: no cover
 
 try:
     from tmuxp.workspace.loader import expand as expand_config
-except ImportError:  # pragma: no cover
+except ImportError:  # pragma: has_tmuxp_1_18
     # In tmuxp < 1.18, `expand` was in `tmuxp.config`
     from tmuxp.config import expand as expand_config
 
 try:
     from tmuxp.workspace.finders import get_workspace_dir
-except ImportError:  # pragma: no cover
+except ImportError:  # pragma: has_tmuxp_1_18
     # In tmuxp < 1.18, `get_workspace_dir` was called `get_config_dir`.
     # In tmuxp >= 1.11, < 1.18 get_config_dir was in `tmuxp.cli.utils`.
     # In tmuxp < 1.11, get_config_dir was in tmuxp.cli.
     try:
         from tmuxp.cli.utils import get_config_dir as get_workspace_dir
-    except ImportError:  # pragma: no cover
+    except ImportError:  # pragma: has_tmuxp_1_11
         from tmuxp.cli import get_config_dir as get_workspace_dir
 
 try:
     from tmuxp.workspace.finders import in_dir as configs_in_dir
-except ImportError:  # pragma: no cover
+except ImportError:  # pragma: has_tmuxp_1_18
     # In tmuxp < 1.18, `in_dir` was in `tmuxp.config`
     from tmuxp.config import in_dir as configs_in_dir
 
