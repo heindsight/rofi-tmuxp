bf725d04c007ef0e1ae28658a155807348f9bbe0
Heinrich Kruger <heindsight@kruger.dev>
2024-09-05 21:16:40 +0100

    Drop support for ancient tmuxp

--- rofi_tmuxp/tmuxp_client.py
+++ rofi_tmuxp/tmuxp_client.py
@@ -4,30 +4,9 @@ from pathlib import Path
 from typing import Any, Dict
 
 import yaml
-
-
-try:
-    from tmuxp.workspace.loader import expand as expand_config
-except ImportError:  # no_cover_tmuxp_gte_1_18
-    # In tmuxp < 1.18, `expand` was in `tmuxp.config`
-    from tmuxp.config import expand as expand_config
-
-try:
-    from tmuxp.workspace.finders import get_workspace_dir
-except ImportError:  # no_cover_tmuxp_gte_1_18
-    # In tmuxp < 1.18, `get_workspace_dir` was called `get_config_dir`.
-    # In tmuxp >= 1.11, < 1.18 get_config_dir was in `tmuxp.cli.utils`.
-    # In tmuxp < 1.11, get_config_dir was in tmuxp.cli.
-    try:
-        from tmuxp.cli.utils import get_config_dir as get_workspace_dir
-    except ImportError:  # no_cover_tmuxp_gte_1_11
-        from tmuxp.cli import get_config_dir as get_workspace_dir
-
-try:
-    from tmuxp.workspace.finders import in_dir as configs_in_dir
-except ImportError:  # no_cover_tmuxp_gte_1_18
-    # In tmuxp < 1.18, `in_dir` was in `tmuxp.config`
-    from tmuxp.config import in_dir as configs_in_dir
+from tmuxp.workspace.finders import get_workspace_dir
+from tmuxp.workspace.finders import in_dir as configs_in_dir
+from tmuxp.workspace.loader import expand as expand_config
 
 
 __all__ = [
