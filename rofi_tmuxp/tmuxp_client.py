f376f55e00baf22941e5fd87113d2e6b5ece7d72
Heinrich Kruger <heindsight@kruger.dev>
2024-09-05 21:06:47 +0100

    Just use pyyaml to  load config
    Newer versions of tmuxp moved ConfigReader to internal API

--- rofi_tmuxp/tmuxp_client.py
+++ rofi_tmuxp/tmuxp_client.py
@@ -2,17 +2,7 @@
 from pathlib import Path
 from typing import Any, Dict
 
-
-try:
-    _have_config_reader = True
-    from tmuxp.config_reader import ConfigReader
-except ImportError:  # no_cover_tmuxp_gte_1_16
-    # In tmuxp < 1.16, 3d party library `Kaptan` was used to read config files.
-    # `Kaptan` is just a wrapper around `PyYAML` and `json`. Since Yaml is a superset of
-    # JSON, we'll just use `yaml.safe_load`.
-    import yaml
-
-    _have_config_reader = False
+import yaml
 
 
 try:
@@ -52,13 +42,6 @@ Config = Dict[str, Any]
 
 
 def read_config_file(config_path: Path) -> Config:
-    """Read a tmuxp session config  file.
-
-    If `tmuxp.config_reader.ConfigReader` is available, use that. Otherwise, fall back
-    to `yaml.safe_load`.
-    """
-    if _have_config_reader:  # no_cover_tmuxp_lt_1_16
-        cfg_reader = ConfigReader.from_file(config_path)
-        return cfg_reader.content
-    else:  # no_cover_tmuxp_gte_1_16
-        return yaml.safe_load(config_path.read_text())
+    """Read a tmuxp session config  file."""
+    cfg: Config = yaml.safe_load(config_path.read_text())
+    return cfg
